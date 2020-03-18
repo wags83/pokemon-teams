c52c59662005356cc0599825cd63054b387b91b5
wags83 <gpwagner@gmail.com>
2020-03-18 10:11:48 -0400

    10:10

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,39 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', () =>{
+    getTrainerData()
+})
+
+function makeTrainerCard(trainer) {
+    const trainerCard = document.createElement('div')
+    trainerCard.className = 'card'
+    trainerCard.dataset.id = trainer.id
+    trainerCard.innerHTML = `
+    <p>${trainer.name}</p>
+    <button data-trainer-id=${trainer.id} class="add-pokemon-button">Add Pokemon</button>
+    <ul>
+    </ul>`
+    document.body.append(trainerCard)
+}
+
+function getTrainerData() {
+    fetch(TRAINERS_URL)
+    .then(response => {
+        return response.json()
+    })
+    .then(result => {
+        console.log(result)
+        result.forEach(element => {
+            makeTrainerCard(element)
+        });
+    })
+}
+
+function addPokemon() {
+    const trainerCard = document.querySelector('.card')
+    const pokemonLI = document.createElement('li')
+
+}
+
